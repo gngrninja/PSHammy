c264428a2d1fff85151e288445878bea49e93b90
gngrninja <thegngrninja@gmail.com>
2019-04-26 22:13:35 -0700

    more maps goodness

--- functions/private/Get-AzureMapsInfo.ps1
+++ functions/private/Get-AzureMapsInfo.ps1
@@ -4,39 +4,79 @@ function Get-AzureMapsInfo {
         [Parameter(
             Mandatory
         )]
-        [ValidateSet('Search')]
+        [ValidateSet('Search','MapPin','SearchAndPin')]
         $RequestType,
         
         [Parameter(
 
         )]
-        $RequestData
+        $RequestData,
+
+        [Parameter(
+
+        )]
+        $DefaultZoom = '5'
     )
 
     begin {
 
+        $Prefix = 'https://atlas.microsoft.com'
+
         $headers = @{
             'x-ms-client-id' = $config.AzureMapsApiKey
         }
-        $baseUrl = "https://atlas.microsoft.com/search/fuzzy/json?api-version=1.0&query=$RequestData"
          
         switch ($RequestType) {
-            '' {
+            'Search' {
                 
             }
             'MapPin' {
-                $baseUrl = "https://atlas.microsoft.com/map/static/png?api-version=1.0&center=-122%2C45&pins=default%7C%7C-122%2045&zoom=6"
+                
             }
         }
     }
 
     process {
 
-        $response = Invoke-RestMethod -Uri $baseUrl -Headers $headers
-        
+        switch ($RequestType){
+            'MapPin' {
+
+                $baseUrl = "$($Prefix)/map/static/png?api-version=1.0&center=$($RequestData)&pins=default%7C%7C-122%2045&zoom=$($DefaultZoom)"
+                $response = Invoke-RestMethod -Uri $baseUrl -Headers $headers -OutFile ".\image.png"
+
+            }
+
+            'Search' {
+
+                $baseUrl = "$($Prefix)/search/fuzzy/json?api-version=1.0&query=$($RequestData)"  
+                $response = Invoke-RestMethod -Uri $baseUrl -Headers $headers
+                
+            }
+
+            'SearchAndPin' {
+
+                $baseUrl = "$($Prefix)/search/fuzzy/json?api-version=1.0&query=$($RequestData)" 
+                $response = Invoke-RestMethod -Uri $baseUrl -Headers $headers
+
+                $firstResult = $response.results[0].position
+
+                $lat = $firstResult.lat
+                $lon = $firstResult.lon
+
+                $response = $null
+                $baseUrl  = $null
+
+                $baseUrl  = "$($Prefix)/map/static/png?api-version=1.0&center=$($lon),$($lat)&pins=default%7C%7C$($lon) $($lat)&zoom=$($DefaultZoom)"
+                Write-Verbose "$baseUrl"
+                $response = Invoke-RestMethod -Uri $baseUrl -Headers $headers -OutFile ".\image.png"
+
+            }
+        }
     }
 
     end {
+
         return $response
+
     }
 }
