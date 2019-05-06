1afc4dc15d1d5927ed1ee53f5baa055db0ad77b7
gngrninja <thegngrninja@gmail.com>
2019-05-06 12:08:34 -0700

    adding ability to run as job

---
+++ PSHammy/functions/private/Invoke-LogCheck.ps1
@@ -0,0 +1,120 @@
+function Invoke-LogCheck {
+    [cmdletbinding()]
+    param(
+
+    )
+    while ($true) {
+    
+        $fromToday = $logData | Where-Object {
+
+            [DateTime]$_.WorkedDate -ge [DateTime]::Now.ToString("yyyy-MM-dd")
+    
+        }
+        
+        if ($fromToday.Count -le 0) {
+
+            Write-HostForScript -Message "No log entries found -> [$($wsjtxLogPath)] -< aborting!"
+            
+            break
+        }
+
+        Write-HostForScript -Message "Found [$($fromToday.Count)] log entries..."
+
+        foreach ($contact in $fromToday) {
+    
+            $theirCallInfo  = $null
+            $pinData        = $null
+            $theirLocation  = $null
+            $guid           = $null
+            $dateTimeWorked = $null
+            $lookupAddy     = $null
+            $theirLat       = $null
+            $theirLong      = $null
+
+            $dateTimeWorked = "$($contact.WorkedDate)$($contact.WorkedTime.Replace(':','-'))"
+            $guid           = "$($contact.WorkedCallSign)-$($dateTimeWorked)"
+            
+            Write-Verbose "Working with GUID -> [$($guid)]..."
+            Write-Verbose "Processed:"
+            Write-Verbose  ($processed| out-string)
+
+            if ($guid -notin $processed) {
+                
+                Write-HostForScript -Message "Looking up call sign information for [$($contact.WorkedCallSign)]..."
+    
+                $theirCallInfo = Invoke-CallSignLookup -CallSign $contact.WorkedCallSign
+
+                if ($theirCallInfo.lat -and $theirCallInfo.long) {
+
+                    $theirLat  = $theirCallInfo.lat
+                    $theirLong = $theirCallInfo.long
+
+                } else {
+
+                    $lookupAddy = "$($theirCallInfo.Addy) $($theirCallInfo.AddyTwo) $($theirCallInfo.State) $($theirCallInfo.Zip)"
+                    $theirLocation = Get-AzureMapsInfo -RequestData $lookupAddy -RequestType 'Search'
+
+                    $theirLat  = $theirLocation.results[0].position.lat
+                    $theirLong = $theirLocation.results[0].position.lon
+
+                }
+               
+                $pinData = [PSCustomObject]@{
+        
+                    MyCall         = $myCallData.CallSign
+                    MyLat          = $myLat
+                    MyLong         = $myLong
+                    TheirCall      = $theirCallInfo.CallSign 
+                    TheirLat       = $theirLat
+                    TheirLong      = $theirLong
+                    Frequency      = $contact.Frequency
+                    DateTimeWorked = $dateTimeWorked
+                    TheirState     = $theirCallInfo.State
+                    MyState        = $myCallData.State
+                    MyRig          = $wsjtxConfig.Configuration.Rig
+                    MyGrid         = $myCallData.Grid
+                    TheirGrid      = $theirCallInfo.Grid
+                    MyImage        = $myCallData.ProfileImage
+                    TheirImage     = $theirCallInfo.ProfileImage
+                    
+                }
+        
+                Write-Verbose "Pin data:"
+                Write-Verbose ($PinData | Out-String)
+        
+                try {                            
+                    
+                    Write-HostForScript -Message "Getting map image for contact..."
+    
+                    $result = Get-AzureMapsInfo -RequestType MapPin -PinData $pinData -FindCenter  
+        
+                    Write-HostForScript -Message "Attempting to send data to Discord..."
+        
+                    Invoke-WebHookSend -PinData $pinData -ContactData $contact -ImagePath $result
+                        
+                }
+                catch {
+        
+                    $errorMessage = $_.Exception.Message
+                    Write-Error "Error -> [$errorMessage]!"
+        
+                }
+                finally {
+        
+                    $processed = Invoke-ProcessedLog -Action Add -FilePath $processedPath -Guid $guid                    
+                    $logData   = Import-WsjtxLog -LogPath $wsjtxLogPath
+
+                    Start-Sleep -Second 7
+        
+                }                                        
+            } else {
+
+                $processed  = Invoke-ProcessedLog -Action Get -FilePath $processedPath                  
+                $logData    = Import-WsjtxLog -LogPath $wsjtxLogPath
+
+                Start-Sleep -Second 2
+
+            }
+        }
+    } 
+}
--- PSHammy/functions/public/Invoke-PSHammy.ps1
+++ PSHammy/functions/public/Invoke-PSHammy.ps1
@@ -1,11 +1,19 @@
 function Invoke-PSHammy {
     [cmdletbinding()]
     param(
+        
         [Parameter(
 
         )]
         [switch]
-        $DoNotAutoDeleteImages
+        $DoNotAutoDeleteImages,
+
+        [Parameter(
+
+        )]
+        [switch]
+        $AsJob
+
     )
           
     if (!(Test-Path -Path $wsjtxLogPath -ErrorAction SilentlyContinue)) {
@@ -77,178 +85,26 @@ function Invoke-PSHammy {
     
         break
     
-    }
-    
-    $script:config = Import-Config -Path $hammyConfigPath
-    $wsjtxConfig   = Get-IniContent -FilePath $wsjtxConfigPath
-    $qrzCreds      = Import-Clixml -Path $qrzCredPath
-    
-    #Get call sign from wsjtx config
-    if ($wsjtxConfig) {
-    
-        $DefaultCall = $wsjtxConfig.Configuration.MyCall
-    
-    }
-    
-    Write-HostForScript -Message "Imported call sign [$DefaultCall] from WSJT-X.ini..."
-    
-    Write-HostForScript -Message "Attempting to import log data from [$wsjtxLogPath]..."
-    
-    $logData = Import-WsjtxLog -LogPath $wsjtxLogPath
-    
-    if ($logData) {
+    }        
         
-        $lookupAddy = $null
-    
-        Write-HostForScript -Message "Attempting to import processed data from [$processedPath]..."
-        Write-HostForScript -Message "Looking up call sign information for [$($DefaultCall)]..."
-    
-        Write-HostForScript -Message "Checking QRZ session status..."
-    
-        $result = Get-QrzApiSession -Credential $qrzCreds
-    
-        if ($result -eq 'keep') {
-    
-            Write-HostForScript -Message "Keeping key for QRZ, as it still works!"
-    
-        } else {
-    
-            Write-HostForScript -Message "Refreshing QRZ API key, old key does not work!"
-    
+    if ($AsJob) {
+
+        Start-Job -InitializationScript {
+
+            Import-Module "C:\Users\thegn\repos\PSHammy\PSHammy"            
+            
+
+        } -ArgumentList $logData, $processed, $myCallData, $myLat, $myLong, $myLocation, $DefaultCall, $config, $wsjtxConfig, $qrzCreds, $DoNotAutoDeleteImages, $hammyConfigPath, $wsjtxConfigPath, $qrzCredPath {
+
+            Invoke-LogDataGather
+            Invoke-LogCheck
+
         }
-    
-        $processed  = Invoke-ProcessedLog -Action Get -FilePath $processedPath     
-        $myCallData = Invoke-CallSignLookup -CallSign $DefaultCall    
-    
-        if ($myCallData.lat -and $myCallData.long) {
-    
-            $myLat  = $myCallData.lat
-            $myLong = $myCallData.long
-    
-        } else {
-    
-            $lookupAddy = "$($myCallData.Addy) $($myCallData.AddyTwo) $($myCallData.State) $($myCallData.Zip)"
-            $myLocation = Get-AzureMapsInfo -RequestData $lookupAddy -RequestType 'Search'
-    
-            $myLat  = $myLocation.results[0].position.lat
-            $myLong = $myLocation.results[0].position.lon 
-    
-        }
-    
-        #As job
-        while ($true) {
-    
-            $fromToday = $logData | Where-Object {
-    
-                [DateTime]$_.WorkedDate -ge [DateTime]::Now.ToString("yyyy-MM-dd")
-        
-            }
-            
-            if ($fromToday.Count -le 0) {
-    
-                Write-HostForScript -Message "No log entries found -> [$($wsjtxLogPath)] -< aborting!"
-                
-                break
-            }
-    
-            Write-HostForScript -Message "Found [$($fromToday.Count)] log entries..."
-    
-            foreach ($contact in $fromToday) {
-        
-                $theirCallInfo  = $null
-                $pinData        = $null
-                $theirLocation  = $null
-                $guid           = $null
-                $dateTimeWorked = $null
-                $lookupAddy     = $null
-                $theirLat       = $null
-                $theirLong      = $null
-    
-                $dateTimeWorked = "$($contact.WorkedDate)$($contact.WorkedTime.Replace(':','-'))"
-                $guid           = "$($contact.WorkedCallSign)-$($dateTimeWorked)"
-                
-                Write-Verbose "Working with GUID -> [$($guid)]..."
-                Write-Verbose "Processed:"
-                Write-Verbose  ($processed| out-string)
-    
-                if ($guid -notin $processed) {
-                    
-                    Write-HostForScript -Message "Looking up call sign information for [$($contact.WorkedCallSign)]..."
-        
-                    $theirCallInfo = Invoke-CallSignLookup -CallSign $contact.WorkedCallSign
-    
-                    if ($theirCallInfo.lat -and $theirCallInfo.long) {
-    
-                        $theirLat  = $theirCallInfo.lat
-                        $theirLong = $theirCallInfo.long
-    
-                    } else {
-    
-                        $lookupAddy = "$($theirCallInfo.Addy) $($theirCallInfo.AddyTwo) $($theirCallInfo.State) $($theirCallInfo.Zip)"
-                        $theirLocation = Get-AzureMapsInfo -RequestData $lookupAddy -RequestType 'Search'
-    
-                        $theirLat  = $theirLocation.results[0].position.lat
-                        $theirLong = $theirLocation.results[0].position.lon
-    
-                    }
-                   
-                    $pinData = [PSCustomObject]@{
-            
-                        MyCall         = $myCallData.CallSign
-                        MyLat          = $myLat
-                        MyLong         = $myLong
-                        TheirCall      = $theirCallInfo.CallSign 
-                        TheirLat       = $theirLat
-                        TheirLong      = $theirLong
-                        Frequency      = $contact.Frequency
-                        DateTimeWorked = $dateTimeWorked
-                        TheirState     = $theirCallInfo.State
-                        MyState        = $myCallData.State
-                        MyRig          = $wsjtxConfig.Configuration.Rig
-                        MyGrid         = $myCallData.Grid
-                        TheirGrid      = $theirCallInfo.Grid
-                        MyImage        = $myCallData.ProfileImage
-                        TheirImage     = $theirCallInfo.ProfileImage
-                        
-                    }
-            
-                    Write-Verbose "Pin data:"
-                    Write-Verbose ($PinData | Out-String)
-            
-                    try {                            
-                        
-                        Write-HostForScript -Message "Getting map image for contact..."
-        
-                        $result = Get-AzureMapsInfo -RequestType MapPin -PinData $pinData -FindCenter  
-            
-                        Write-HostForScript -Message "Attempting to send data to Discord..."
-            
-                        Invoke-WebHookSend -PinData $pinData -ContactData $contact -ImagePath $result
-                            
-                    }
-                    catch {
-            
-                        $errorMessage = $_.Exception.Message
-                        Write-Error "Error -> [$errorMessage]!"
-            
-                    }
-                    finally {
-            
-                        $processed = Invoke-ProcessedLog -Action Add -FilePath $processedPath -Guid $guid                    
-                        $logData   = Import-WsjtxLog -LogPath $wsjtxLogPath
-    
-                        Start-Sleep -Second 7
-            
-                    }                                        
-                } else {
-    
-                    $processed  = Invoke-ProcessedLog -Action Get -FilePath $processedPath                  
-                    $logData    = Import-WsjtxLog -LogPath $wsjtxLogPath
-    
-                    Start-Sleep -Second 2
-    
-                }
-            }
-        }    
-     }    
-}
+
+    } else {
+
+        Invoke-LogDataGather
+        Invoke-LogCheck
+
+    }  
+}    
